e93a8c5d13d1b33d8ac3722ba830632490273e92
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-02-23 18:27:14 +0530

    Update PhonePe_DashBoard.py

--- PhonePe_DashBoard.py
+++ PhonePe_DashBoard.py
@@ -8,11 +8,15 @@ from plotly.subplots import make_subplots
 warnings.filterwarnings("ignore")
 st. set_page_config(layout="wide")
 
+username=st.secrets['AWS_RDS_username']
+password=st.secrets['AWS_RDS_password']
+Endpoint=st.secrets['Endpoint']
+
 # CONNECTED TO (AWS)Amazon-Web-Services ----> (RDS)Relational-Database-Service 
 conn=pymysql.connect(
-    host='phonepedatabase.cw0dknqm0t6h.us-east-1.rds.amazonaws.com',
-    user='admin',
-    password='12345phone'
+    host=Endpoint,
+    user=username,
+    password=password
 )
 
 # USING PHONEPE DATABASE I HAVE CREATED IN DATABASE-INSTANCE OF RDS
@@ -32,7 +36,7 @@ Scatter_Geo_Dataset =  pd.read_csv(r'data/Data_Map_Districts_Longitude_Latitude.
 Coropleth_Dataset =  pd.read_csv(r'data/Data_Map_IndiaStates_TU.csv')
 Data_Map_Transaction_df = pd.read_csv(r'data/Data_Map_Transaction_Table.csv')
 Data_Map_User_Table= pd.read_csv(r'data/Data_Map_User_Table.csv')
-Indian_States= pd.read_csv(r'data/Longitude_Latitude_State_Table.csv')
+#Indian_States= pd.read_csv(r'data/Longitude_Latitude_State_Table.csv')
 colT1,colT2 = st.columns([2,8])
 with colT2:
     st.title(':red[PhonePe Pulse Data Analysis:signal_strength:]')
